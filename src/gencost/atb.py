92ec0bacf29d26e575be3ae28ee608b7e9c333db
Alex Engel <arengel@users.noreply.github.com>
2023-05-18 00:08:56 -0600

    adding atb processing for proposed and missing plants, moving carb and cost floor constants from patio

---
+++ src/gencost/atb.py
@@ -0,0 +1,175 @@
+from functools import lru_cache
+
+import pandas as pd
+
+from gencost.constants import CARB_INTENSITY, COST_FLOOR
+
+TECH_MAP = {
+    "Biopower_Dedicated": "Wood/Wood Waste Biomass",
+    "Coal_FE_newAvgCF": "Conventional Steam Coal",
+    # "Geothermal_HydroFlash": "Geothermal",
+    # "Hydropower_NPD1": "Conventional Hydroelectric",
+    # "Hydropower_NPD2": "Conventional Hydroelectric",
+    # "Hydropower_NPD3": "Conventional Hydroelectric",
+    # "Hydropower_NPD4": "Conventional Hydroelectric",
+    # "Hydropower_NPD5": "Conventional Hydroelectric",
+    # "Hydropower_NPD6": "Conventional Hydroelectric",
+    # "Hydropower_NPD7": "Conventional Hydroelectric",
+    # "Hydropower_NPD8": "Conventional Hydroelectric",
+    # "Hydropower_NSD1": "Conventional Hydroelectric",
+    # "Hydropower_NSD2": "Conventional Hydroelectric",
+    # "Hydropower_NSD3": "Conventional Hydroelectric",
+    # "Hydropower_NSD4": "Conventional Hydroelectric",
+    # "LandbasedWind_Class1": "Onshore Wind Turbine",
+    # "LandbasedWind_Class10": "Onshore Wind Turbine",
+    # "LandbasedWind_Class2": "Onshore Wind Turbine",
+    # "LandbasedWind_Class3": "Onshore Wind Turbine",
+    # "LandbasedWind_Class4": "Onshore Wind Turbine",
+    # "LandbasedWind_Class5": "Onshore Wind Turbine",
+    # "LandbasedWind_Class6": "Onshore Wind Turbine",
+    # "LandbasedWind_Class7": "Onshore Wind Turbine",
+    # "LandbasedWind_Class8": "Onshore Wind Turbine",
+    # "LandbasedWind_Class9": "Onshore Wind Turbine",
+    "NaturalGas_FE_CCAvgCF": "Natural Gas Fired Combined Cycle",
+    "NaturalGas_FE_CTAvgCF": "Natural Gas Fired Combustion Turbine",
+    # "Nuclear_Nuclear": "Nuclear",
+    # "OffShoreWind_Class1": "Offshore Wind Turbine",
+    # "OffShoreWind_Class10": "Offshore Wind Turbine",
+    # "OffShoreWind_Class11": "Offshore Wind Turbine",
+    # "OffShoreWind_Class12": "Offshore Wind Turbine",
+    # "OffShoreWind_Class13": "Offshore Wind Turbine",
+    # "OffShoreWind_Class14": "Offshore Wind Turbine",
+    # "OffShoreWind_Class2": "Offshore Wind Turbine",
+    # "OffShoreWind_Class3": "Offshore Wind Turbine",
+    # "OffShoreWind_Class4": "Offshore Wind Turbine",
+    # "OffShoreWind_Class5": "Offshore Wind Turbine",
+    # "OffShoreWind_Class6": "Offshore Wind Turbine",
+    # "OffShoreWind_Class7": "Offshore Wind Turbine",
+    # "OffShoreWind_Class8": "Offshore Wind Turbine",
+    # "OffShoreWind_Class9": "Offshore Wind Turbine",
+    # "Pumped Storage Hydropower_NatlClass1": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass10": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass11": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass12": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass13": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass14": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass15": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass2": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass3": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass4": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass5": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass6": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass7": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass8": "Hydroelectric Pumped Storage",
+    # "Pumped Storage Hydropower_NatlClass9": "Hydroelectric Pumped Storage",
+    # "Utility-Scale Battery Storage_10Hr Battery Storage": "Batteries",
+    # "Utility-Scale Battery Storage_2Hr Battery Storage": "Batteries",
+    # "Utility-Scale Battery Storage_4Hr Battery Storage": "Batteries",
+    # "Utility-Scale Battery Storage_6Hr Battery Storage": "Batteries",
+    # "Utility-Scale Battery Storage_8Hr Battery Storage": "Batteries",
+    # "UtilityPV_Class1": "Solar Photovoltaic",
+    # "UtilityPV_Class10": "Solar Photovoltaic",
+    # "UtilityPV_Class2": "Solar Photovoltaic",
+    # "UtilityPV_Class3": "Solar Photovoltaic",
+    # "UtilityPV_Class4": "Solar Photovoltaic",
+    # "UtilityPV_Class5": "Solar Photovoltaic",
+    # "UtilityPV_Class6": "Solar Photovoltaic",
+    # "UtilityPV_Class7": "Solar Photovoltaic",
+    # "UtilityPV_Class8": "Solar Photovoltaic",
+    # "UtilityPV_Class9": "Solar Photovoltaic",
+}
+VAR_MAP = {
+    "Fixed O&M": "fom_per_kw",
+    "Fuel": "fuel_per_mwh",
+    "Variable O&M": "vom_per_mwh",
+    "CAPEX": "capex_per_kw",
+}
+HR = {
+    "Wood/Wood Waste Biomass": 13.5,  # ATB 2022 v2 Biopower Dedicated
+    "Conventional Steam Coal": 8.48894,  # ATB 2022 v2 Coal-new
+    "Natural Gas Fired Combined Cycle": 6.363,  # ATB 2022 v2 NG F-Frame CC
+    "Natural Gas Fired Combustion Turbine": 9.717,  # ATB 2022 v2 NG F-Frame CT
+    "Nuclear": 10.461,  # ATB 2022 v2
+}
+
+
+@lru_cache
+def dl_atb():
+    return (
+        pd.read_parquet(
+            "https://oedi-data-lake.s3.amazonaws.com/ATB/electricity/parquet/2022/ATBe.parquet"
+        )
+        .query(
+            "core_metric_case == 'Market' "
+            "& core_metric_parameter in @VAR_MAP "
+            "& scenario == 'Moderate' "
+            "& crpyears  == '30'"
+        )
+        .assign(
+            technology_description=lambda x: x.technology.str.cat(
+                x.techdetail, sep="_"
+            ).map(TECH_MAP),
+            variable=lambda x: x.core_metric_parameter.map(VAR_MAP),
+            year=lambda x: x.core_metric_variable.astype(int),
+            start_per_kw=0,
+            heat_rate=lambda x: x.technology_description.map(HR),
+            co2_factor=lambda x: x.technology_description.map(CARB_INTENSITY),
+        )
+        .dropna(subset="technology_description")
+        .pivot(
+            index=[
+                "technology_description",
+                "techdetail",
+                "year",
+                "start_per_kw",
+                "heat_rate",
+                "co2_factor",
+            ],
+            columns="variable",
+            values="value",
+        )
+        .reset_index()
+        .assign(
+            fuel_per_mwh=lambda x: x.fuel_per_mwh.fillna(
+                x.technology_description.map(
+                    COST_FLOOR.set_index("technology_description").fuel_floor.to_dict()
+                )
+            ),
+        )
+    )
+
+
+def get_atb(proposed, years=(2008, 2021)):
+    atb = dl_atb().merge(
+        proposed.assign(
+            year_=lambda x: x.operating_date.dt.year,
+            year=lambda x: x.year_.mask(x.year_ < 2020, 2020),
+        ).reset_index()[
+            [
+                "plant_id_eia",
+                "generator_id",
+                "year",
+                "technology_description",
+                "final_ba_code",
+            ]
+        ],
+        on=["technology_description", "year"],
+        how="inner",
+        validate="1:m",
+    )
+    dt_range = pd.to_datetime(range(*years), format="%Y")
+    return (
+        pd.concat(atb.assign(datetime=dt) for dt in dt_range)
+        .set_index(["plant_id_eia", "generator_id", "datetime"])
+        .sort_index()[
+            [
+                "final_ba_code",
+                "vom_per_mwh",
+                "fuel_per_mwh",
+                "fom_per_kw",
+                "start_per_kw",
+                "heat_rate",
+                "co2_factor",
+            ]
+        ]
+    )
--- src/gencost/constants.py
+++ src/gencost/constants.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 FOSSIL_PRIME_MOVER_MAP = {
     "ST": "ST",  # steam turbine
     "GT": "GT",  # gas turbine xCC
@@ -8,6 +10,7 @@ FOSSIL_PRIME_MOVER_MAP = {
     "CC": "CC",  # CC in planning
 }
 UDAY_FOSSIL_FUEL_MAP = {  # the better map from Uday
+    "ANT": "coal",
     "BIT": "coal",
     "LIG": "coal",
     "SGC": "coal",
@@ -25,15 +28,16 @@ UDAY_FOSSIL_FUEL_MAP = {  # the better map from Uday
     "BFG": "other_gas",
     "NG": "natural_gas",
     "OG": "other_gas",
+    "SC": "coal",
+    "TDF": "petroleum",
 }
 FUEL_GROUP_MAP = UDAY_FOSSIL_FUEL_MAP | {
-    "ANT": "coal",
     "AB": "biofuel",  # other in fuel_group_emissions_map
-    "MSW": "biofuel",  # other in fuel_group_emissions_map
+    "MSW": "other",  # other in fuel_group_emissions_map
     "OBS": "biofuel",  # other in fuel_group_emissions_map
     "WDS": "biofuel",  # other in fuel_group_emissions_map
     "OBL": "biofuel",  # other in fuel_group_emissions_map
-    "SLW": "biofuel",  # other in fuel_group_emissions_map
+    "SLW": "other",  # other in fuel_group_emissions_map
     "BLQ": "biofuel",  # other in fuel_group_emissions_map
     "WDL": "biofuel",  # other in fuel_group_emissions_map
     "LFG": "biofuel",  # other in fuel_group_emissions_map
@@ -45,13 +49,64 @@ FUEL_GROUP_MAP = UDAY_FOSSIL_FUEL_MAP | {
     "WAT": "renew",  # other in fuel_group_emissions_map
     "NUC": "nuclear",  # other in fuel_group_emissions_map
     "WH": "other",
-    "TDF": "other",
     "MWH": "other",
     "OTH": "other",
     "MSN": "other",
     "PUR": "other",
-    "SC": "coal",
 }
+CARB_INTENSITY = {
+    "Petroleum Liquids": 0.07509,
+    "Natural Gas Steam Turbine": 0.05291,
+    "Conventional Steam Coal": 0.09610,
+    "Natural Gas Fired Combined Cycle": 0.05291,
+    "Natural Gas Fired Combustion Turbine": 0.05291,
+    "Natural Gas Internal Combustion Engine": 0.05291,
+    "Coal Integrated Gasification Combined Cycle": 0.09610,
+    "Other Natural Gas": 0.05291,
+    "Petroleum Coke": 0.10212,
+    "Natural Gas with Compressed Air Storage": 0.05291,
+    "Other Gases": 0.06288,
+    "Solar Photovoltaic": 0.0,
+    "Onshore Wind Turbine": 0.0,
+    "Offshore Wind Turbine": 0.0,
+    "Solar": 0.0,
+    "Wind": 0.0,
+    "Offshore_Wind": 0.0,
+    "Wood/Wood Waste Biomass": 0.04989,  # same as MSW
+    "Municipal Solid Waste": 0.04989,  # same as MSW
+    "Landfill Gas": 0.04989,  # same as MSW
+    "All Other": 0.06288,  # same as propane / other gases
+    "Other Waste Biomass": 0.04989,  # same as MSW
+}
+"""
+https://www.eia.gov/environment/emissions/co2_vol_mass.php
+
+in tonnes/mmbtu
+"""
+COST_FLOOR = pd.DataFrame(
+    [
+        ("Coal Integrated Gasification Combined Cycle", 5.0, 35.0, 0.1, 10.0),
+        ("Conventional Steam Coal", 4.0, 25.0, 0.1, 10.0),
+        ("Landfill Gas", 4.0, 15.0, 0.1, 10.0),
+        ("Municipal Solid Waste", 4.0, 15.0, 0.1, 10.0),
+        ("Natural Gas Fired Combined Cycle", 2.0, 20.0, 0.1, 15.0),
+        ("Natural Gas Fired Combustion Turbine", 5.0, 15.0, 0.1, 20.0),
+        ("Natural Gas Internal Combustion Engine", 4.0, 15.0, 0.1, 15.0),
+        ("Natural Gas Steam Turbine", 4.0, 20.0, 0.1, 20.0),
+        ("Other Gases", 5.0, 15.0, 0.1, 10.0),
+        ("Other Waste Biomass", 4.0, 15.0, 0.1, 10.0),
+        ("Petroleum Coke", 5.0, 15.0, 0.1, 30.0),
+        ("Petroleum Liquids", 5.0, 15.0, 0.1, 40.0),
+        ("Wood/Wood Waste Biomass", 4.0, 15.0, 0.1, 10.0),
+    ],
+    columns=[
+        "technology_description",
+        "vom_floor",
+        "fom_floor",
+        "som_floor",
+        "fuel_floor",
+    ],
+)
 
 GET_860_GEN_COLS = [
     "utility_id_eia",
