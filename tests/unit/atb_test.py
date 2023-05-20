7b539ac1e0325f29944d03f3eb13c678ced0224f
Alex Engel <arengel@users.noreply.github.com>
2023-05-20 15:11:42 -0600

    revise `get_atb` to be more general and include RE costs

--- tests/unit/atb_test.py
+++ tests/unit/atb_test.py
@@ -8,12 +8,21 @@ def test_get_atb():
     out = get_atb(
         pd.DataFrame(
             {
-                "plant_id_eia": [1],
-                "generator_id": ["1"],
-                "operating_date": pd.to_datetime(2026, format="%Y"),
-                "technology_description": ["Natural Gas Fired Combined Cycle"],
-                "final_ba_code": ["FPC"],
+                "plant_id_eia": list(range(6)),
+                "generator_id": ["1"] * 6,
+                "operating_date": pd.to_datetime(
+                    [2026, 2030, 2030, 2022, 2025, 2018], format="%Y"
+                ),
+                "technology_description": [
+                    "Natural Gas Fired Combined Cycle",
+                    "Solar Photovoltaic",
+                    "Solar Photovoltaic",
+                    "Nuclear",
+                    "Offshore Wind Turbine",
+                    "Onshore Wind Turbine",
+                ],
+                "final_ba_code": ["FPC"] * 6,
             }
-        )
+        ).set_index(["plant_id_eia", "generator_id"])
     )
     assert not out.empty
